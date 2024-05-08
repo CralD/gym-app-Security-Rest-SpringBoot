bb1edcaf1b7f8fa165cda97375e823e8819e1e9a
Adan.osorio <adaneog@hotmail.com>
2024-05-08 12:18:53 -0500

    Initial commit

---
+++ src/main/java/com/epam/gymappHibernate/GymAppHibernateApplication.java
@@ -0,0 +1,13 @@
+package com.epam.gymappHibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymAppHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymAppHibernateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/epam/gymappHibernate/GymAppHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epam.gymappHibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymAppHibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
