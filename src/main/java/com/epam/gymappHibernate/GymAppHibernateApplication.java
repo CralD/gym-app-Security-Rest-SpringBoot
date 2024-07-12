4f5f9f4e215811cbba8badfb6edde6a6366c79dc
Adan.osorio <adaneog@hotmail.com>
2024-07-12 13:34:32 -0500

    added encryption to properties files sensitive values

--- src/main/java/com/epam/gymappHibernate/GymAppHibernateApplication.java
+++ src/main/java/com/epam/gymappHibernate/GymAppHibernateApplication.java
@@ -1,9 +1,11 @@
 package com.epam.gymappHibernate;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableEncryptableProperties
 public class GymAppHibernateApplication {
 
 	public static void main(String[] args) {
